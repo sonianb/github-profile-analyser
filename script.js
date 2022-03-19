ee5f7e16db39b0e1a43b584f6a8f8201d1b3c545
sonianb <sonia.botsarova@gmail.com>
2022-03-19 20:35:15 +0000

    display total number of starred repos

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const displayStarredRepos = document.getElementById('display-starred-repos')
+
 async function searchUser(username) {
     const response = await fetch(`https://api.github.com/users/${username}`)
     const usernameData = await response.json();
@@ -10,4 +12,22 @@ async function searchUser(username) {
     }
 }
 
-searchUser('sonianb');
+searchUser('sonianb').then(console.log);
+
+async function getRepos(username) {
+    const response = await fetch(`https://api.github.com/users/${username}/starred`)
+    const starredRepos = await response.json();
+
+
+    const totalStarred = starredRepos.length;
+    if (!response.ok) {
+        const message = `Oops, something went wrong: ${response.status}`;
+        throw new Error(message);
+    }
+    else {
+        return `${username} has starred ${totalStarred} repos: 
+        `;
+    }
+}
+
+getRepos('kmj673 ').then(console.log);
